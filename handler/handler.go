f537f8ef572ecebdf9b36a3757366b0b2374ffa9
dkj <Y30221055@mail.ecust.edu.cn>
2024-09-09 00:51:11 +0800

    feat/add file upload and file metadata

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,14 @@
 package handler
 
 import (
+	"cloud-storage/meta"
+	"cloud-storage/utils"
+	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 func UploadHandler(w http.ResponseWriter, r *http.Request) {
@@ -17,5 +22,49 @@ func UploadHandler(w http.ResponseWriter, r *http.Request) {
 		w.Write(data)
 	} else if r.Method == "POST" {
 		//接受文件流及存储到本地目录
+		file, head, err := r.FormFile("file")
+		if err != nil {
+			fmt.Printf("Failed to get data, err:%s\n", err.Error())
+			return
+		}
+		defer file.Close()
+		fileMeta := meta.FileMeta{
+			FileName: head.Filename,
+			Location: "/tmp/" + head.Filename,
+			UploadAt: time.Now().Format("2006-01-02 15:04:05"),
+		}
+
+		newFile, err := os.Create("/tmp/" + head.Filename)
+		if err != nil {
+			fmt.Printf("Failed to create file, err:%s\n", err.Error())
+			return
+		}
+		defer newFile.Close()
+		fileMeta.FileSize, err = io.Copy(newFile, file)
+		if err != nil {
+			fmt.Printf("Failed to save data into file, err:%s\n", err.Error())
+			return
+		}
+
+		newFile.Seek(0, 0)
+		fileMeta.FileSha1 = utils.FileSha1(newFile)
+		meta.UpdateFileMeta(fileMeta)
+		http.Redirect(w, r, "/file/upload/suc", http.StatusFound)
 	}
+}
+
+func UploadSucHand(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Upload finished!")
+}
+
+func GetFileMetaHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	filehash := r.Form["filehash"][0]
+	fMeta := meta.GetFileMeta(filehash)
+	data, err := json.Marshal(fMeta)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
 }
